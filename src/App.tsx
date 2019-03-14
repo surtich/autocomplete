25e5cf4dee1d290bbd691f8ba42f44959732a341
Javier Pérez Arteaga <surtich@gmail.com>
2019-03-14 01:43:23 +0100

    Using RxJs to fix all problemns

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { from, Subject } from "rxjs";
+import { debounceTime, switchMap, tap } from "rxjs/operators";
 import "./App.css";
 import allVillages from "./villages.json";
 
@@ -21,33 +23,61 @@ const search = (village: string): Promise<Village[]> =>
       Math.floor(Math.random() * 3) * 1000 + 2000
     )
   );
+
 const Village = ({ id, name }: Village) => (
   <li style={{ width: "300px", textAlign: "left" }} key={id}>
     <span>{name}</span>
   </li>
 );
+
 const Villages = ({ villages }: { villages: Village[] }) => (
   <ul>{villages.map(Village)}</ul>
 );
 
 class App extends React.Component {
   state = {
+    loading: false,
     village: "",
     villages: []
   };
 
+  village$: Subject<string>;
+
+  constructor(props: any) {
+    super(props);
+    this.village$ = new Subject();
+
+    this.village$
+      .pipe(
+        tap(village => {
+          this.setState({
+            loading: true,
+            village
+          });
+        }),
+        debounceTime(250),
+        tap(() => console.log("Starting search....")),
+        switchMap(village => from(search(village)))
+      )
+      .subscribe(villages => {
+        this.setState({
+          loading: false,
+          villages
+        });
+      });
+  }
+
   handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const village = e.target.value;
-    this.setState({ village });
-    search(village).then(results => {
-      this.setState({
-        villages: results
-      });
-    });
+    this.village$.next(village);
   };
 
+  componentWillUnmount() {
+    this.village$.unsubscribe();
+  }
+
   render() {
-    const { village, villages } = this.state;
+    const { loading, village, villages } = this.state;
     return (
       <div className="App">
         <input
@@ -56,6 +86,7 @@ class App extends React.Component {
           onChange={this.handleChange}
           value={village}
         />
+        <div>{loading && <span>loading...</span>}</div>
         <div>{villages.length && <span>Total: {villages.length}</span>}</div>
         <Villages villages={villages} />
       </div>
